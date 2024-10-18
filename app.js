e5f71588a5444ae5591b66f7dfd429a98df6f5f6
CattusPhysicus <crudastudia@gmail.com>
2024-10-19 01:35:23 +0500

    connection to mongo.db

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const url = 'mongodb://localhost:27017/main';
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(url);
+client.connect();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('This is the list of films')
+  res.send('This is the list of movies');
 })
 
-app.post('/addfilm', (req, res) => {
-  res.send('Film added')
+app.post('/movies', async (req, res) => {
+  try {
+    await client.db('main').collection('movies').insertOne(req.body);
+    return res.status(201).send('movie created');
+  } catch {
+    return res.status(400).json('bad request');
+  }
 })
 
-app.put('/changefilm', (req, res) => {
-  res.send('Film changed')
+app.put('/changemovie', (req, res) => {
+  res.send('movie changed');
 })
 
-app.delete('/delfilm', (req, res) => {
-  res.send('Film was deleted')
+app.delete('/delmovie', (req, res) => {
+  res.send('movie was deleted');
+})
+
+app.post('/categories', async (req, res) => {
+  try {
+    await client.db('main').collection('categories').insertOne(req.body);
+    return res.status(201).send('category created');
+  } catch {
+    return res.status(400).json('bad request');
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
